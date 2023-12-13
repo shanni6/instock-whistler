349d530a68ccc28994aa2966b7f855769ed6df03
Navpreet Virk <nvirkcom@gmail.com>
2023-12-13 15:43:47 -0800

    fix: fix router and files and folder names

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,22 @@
 import "./App.scss";
-import { useState } from 'react'
-import { Route,Routes, BrowserRouter} from "react-router-dom";
 
-import WarehousesPage from "./pages/warehouse/warehouse";
-import InventoryPage from './pages/inventory/inventory';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-
+import WarehousesPage from "./pages/Warehouse/Warehouse";
+import InventoryPage from "./pages/Inventory/Inventory";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/warehouses" />} />
+          <Route path="inventory" element={<InventoryPage />} />
+          <Route path="/warehouses" element={<WarehousesPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App;
