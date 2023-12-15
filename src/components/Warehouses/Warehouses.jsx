8d3d027c8622d09fe9dde0415fcfe812b6b34ac2
shanni6 <240686886@qq.com>
2023-12-14 19:17:22 -0800

    emily-o23w-9: create warehouses page for mobile

--- src/components/Warehouses/Warehouses.jsx
+++ src/components/Warehouses/Warehouses.jsx
@@ -1,9 +1,23 @@
 import "./Warehouses.scss";
 
-import { Link } from "react-router-dom";
+import deleteOutline from "./../../assets/icons/delete_outline-24px.svg";
+import edit from "./../../assets/icons/edit-24px.svg";
 
 function Warehouses() {
     const warehousesData = [
+        {
+            warehouseDetails: {
+                warehouseName: "Manhattan",
+                streetAddress: "503 Broadway",
+                city: "New York",
+                country: "USA",
+            },
+            contactDetails: {
+                contactName: "Parmin Aujla",
+                phone: "+1(629) 555-0129",
+                email: "paujla@instock.com",
+            },
+        },
         {
             warehouseDetails: {
                 warehouseName: "Manhattan",
@@ -22,26 +36,55 @@ function Warehouses() {
     const tbodyRows = warehousesData.map(
         ({ warehouseDetails, contactDetails }) => {
             return (
-                <tr key="">
-                    <td>{warehouseDetails.warehouseName}</td>
-                    <td>{`${warehouseDetails.streetAddress}, ${warehouseDetails.city}, ${warehouseDetails.country}`}</td>
-                    <td>{contactDetails.contactName}</td>
-                    <td>
-                        {contactDetails.phone} {contactDetails.email}
+                <tr
+                    className="warehouses-table__tr warehouses-table__tr--tbody"
+                    key=""
+                >
+                    <div className="warehouses-table__td-container warehouses-table__td-container--warehouse-name">
+                        <td className="warehouses-table__td warehouses-table__td--warehouse-name">
+                            {warehouseDetails.warehouseName}
+                        </td>
+                    </div>
+                    <div className="warehouses-table__td-container warehouses-table__td-container--address">
+                        <td className="warehouses-table__td  warehouses-table__td--address">{`${warehouseDetails.streetAddress}, ${warehouseDetails.city}, ${warehouseDetails.country}`}</td>
+                    </div>
+                    <div className="warehouses-table__td-container warehouses-table__td-container--contact-name">
+                        <td className="warehouses-table__td  warehouses-table__td--contact-name">
+                            {contactDetails.contactName}
+                        </td>
+                    </div>
+                    <div className="warehouses-table__td-container warehouses-table__td-container--contact-information">
+                        <td className="warehouses-table__td  warehouses-table__td--contact-information">
+                            {contactDetails.phone} {contactDetails.email}
+                        </td>
+                    </div>
+                    <td className="warehouses-table__td  warehouses-table__td--actions">
+                        <img src={deleteOutline} />
+                        <img src={edit} />
                     </td>
-                    <td></td>
                 </tr>
             );
         }
     );
 
     return (
-        <section>
-            <h1>Warehouses</h1>
-            <input placeholder="Search..." type="search" />
-            <Link to="">Add New Warehouse</Link>
-            <table>
-                <thead>
+        <section className="warehouses">
+            <h1 className="warehouses__page-header">Warehouses</h1>
+            <div className="warehouses__row">
+                <input
+                    className="warehouses__search"
+                    placeholder="Search..."
+                    type="search"
+                />
+                <button
+                    className="warehouses__button warehouses__button--primary"
+                    to=""
+                >
+                    Add New Warehouse
+                </button>
+            </div>
+            <table className="warehouses-table">
+                <thead className="warehouses-table__thead">
                     <tr>
                         <th>Warehouse</th>
                         <th>Address</th>
