1d0f1300ac3858c6188b16d420c3976c4eff29d4
BettyNiu <bettychaoranniu@gmail.com>
2023-12-15 16:40:53 -0800

    stlye fix and data got from api

--- src/components/InventoryList/InventoryList.jsx
+++ src/components/InventoryList/InventoryList.jsx
@@ -5,113 +5,140 @@ import sortIcon from "../../assets/icons/sort-24px.svg";
 import searchIcon from "../../assets/icons/search-24px.svg";
 import InventoryListCard from "../InventoryListCard/InventoryListCard";
 
-import React, { useState, useEffect } from 'react';
-
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const InventoryList = () => {
+  const [inventoryList, setInventoryList] = useState([]);
+
+  const getList = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/api/inventories");
+      setInventoryList(response.data);
+    } catch (error) {
+      console.error("Error fetching inventory list:", error);
+    }
+  };
+
+  useEffect(() => {
+    getList();
+  }, []);
+
   return (
-    
     <>
-    <section className="inventory">
-      <div className="inventory__nav">
-        <h1 className="inventory__title">Inventory</h1>
-        <div className="inventory__nav-search">
-          <div className="inventory__nav-search-container">
-            <input
-              className="inventory__nav-search-input"
-              type="text"
-              placeholder="Search..."
-            />
+      <section className="inventory">
+        <div className="inventory__nav">
+          <h1 className="inventory__title">Inventory</h1>
+          <div className="inventory__nav-search">
+            <div className="inventory__nav-search-container">
+              <input
+                className="inventory__nav-search-input"
+                type="text"
+                placeholder="Search..."
+              />
               <img
                 className="inventory__nav-search-icon"
                 src={searchIcon}
                 alt="search-icon"
               />
+            </div>
+            <button className="inventory__nav-search-button">
+              + Add New Item
+            </button>
           </div>
-          <button className="inventory__nav-search-button">+ Add New Item</button>
         </div>
-      </div>
 
-      <div className="inventory__container">
-        <ul className="inventory__sort">
-          <li className="inventory__sort-li">
-            <h4 className="inventory__sort-heading">inventory item</h4>
-            <img
-              className="inventory__sort-icon"
-              src={sortIcon}
-              alt="sortIcon"
-            />
-          </li>
-          <li className="inventory__sort-li">
-            <h4 className="inventory__sort-heading">category</h4>
-            <img
-              className="inventory__sort-icon"
-              src={sortIcon}
-              alt="sortIcon"
-            />
-          </li>
-          <li className="inventory__sort-li">
-            <h4 className="inventory__sort-heading">Status</h4>
-            <img
-              className="inventory__sort-icon"
-              src={sortIcon}
-              alt="sortIcon"
-            />
-          </li>
-          <li className="inventory__sort-li">
-            <h4 className="inventory__sort-heading">Qty</h4>
-            <img
-              className="inventory__sort-icon"
-              src={sortIcon}
-              alt="sortIcon"
-            />
-          </li>
-          <li className="inventory__sort-li">
-            <h4 className="inventory__sort-heading">warehouse</h4>
-            <img
-              className="inventory__sort-icon"
-              src={sortIcon}
-              alt="sortIcon"
-            />
-          </li>
-          <li className="inventory__sort-li">
-            <h4 className="inventory__sort-heading">actions</h4>
-          </li>
-        </ul>
-{/* ---------------- */}
-        <ul className="inventory__list">
-          <li className="inventory__list-li product">Television &gt;</li>
-          <li className="inventory__list-li">Electronics </li>
-          <li className="inventory__list-li inventory__list-stock">
-            in stock
-          </li>
-          <li className="inventory__list-li inventory__list-quantity">500 </li>
-          <li className="inventory__list-li inventory__list-location">
-            Manhattan
-          </li>
-          <li className="inventory__list-li inventory__list-actions">
-            <img
-              src={deleteIcon}
-              className="inventory__deleteIcon"
-              alt="deleteIcon"
-            />
-            <img
-              src={editIcon}
-              className="inventory__editIcon"
-              alt="editIcon"
-            />
-          </li>
-        </ul>
-      
-      </div>
+        <div className="inventory__container">
+          <ul className="inventory__sort">
+            <li className="inventory__sort-li">
+              <h4 className="inventory__sort-heading">inventory item</h4>
+              <img
+                className="inventory__sort-icon"
+                src={sortIcon}
+                alt="sortIcon"
+              />
+            </li>
+            <li className="inventory__sort-li">
+              <h4 className="inventory__sort-heading">category</h4>
+              <img
+                className="inventory__sort-icon"
+                src={sortIcon}
+                alt="sortIcon"
+              />
+            </li>
+            <li className="inventory__sort-li">
+              <h4 className="inventory__sort-heading">Status</h4>
+              <img
+                className="inventory__sort-icon"
+                src={sortIcon}
+                alt="sortIcon"
+              />
+            </li>
+            <li className="inventory__sort-li">
+              <h4 className="inventory__sort-heading">Qty</h4>
+              <img
+                className="inventory__sort-icon"
+                src={sortIcon}
+                alt="sortIcon"
+              />
+            </li>
+            <li className="inventory__sort-li">
+              <h4 className="inventory__sort-heading">warehouse</h4>
+              <img
+                className="inventory__sort-icon"
+                src={sortIcon}
+                alt="sortIcon"
+              />
+            </li>
+            <li className="inventory__sort-li">
+              <h4 className="inventory__sort-heading">actions</h4>
+            </li>
+          </ul>
+          {/* ---------------- */}
 
-    </section>
+          {inventoryList?.map((item)=>(
+            <ul className="inventory__list" key={item.id}>
+            <Link to={`xxxxxxxxxxxxxxxx`}>
+            <li className="inventory__list-li product">
+              {item.item_name} &gt;
+            </li>
+            </Link>
+            
+            <li className="inventory__list-li">{item.category} </li>
+            <li className={`inventory__list-li inventory__list-stock ${item.status.toLowerCase() === 'out of stock' ? 'out-of-stock' : ''}`}>
+  {item.status}
+</li>
 
-    <InventoryListCard/>
+            <li className="inventory__list-li inventory__list-quantity">
+              {item.quantity}
+            </li>
+            <li className="inventory__list-li inventory__list-location">
+              {item.warehouse_id}
+            </li>
+            <li className="inventory__list-li inventory__list-actions">
+              <img
+                src={deleteIcon}
+                className="inventory__deleteIcon"
+                alt="deleteIcon"
+              />
+              <img
+                src={editIcon}
+                className="inventory__editIcon"
+                alt="editIcon"
+              />
+            </li>
+          </ul>
+
+          ))
+
+          }
+          
+        </div>
+      </section>
+
+      <InventoryListCard inventoryList={inventoryList}/>
     </>
-    
-
-    
   );
 };
 export default InventoryList;
--- src/components/InventoryListCard/InventoryListCard.jsx
+++ src/components/InventoryListCard/InventoryListCard.jsx
@@ -4,122 +4,68 @@ import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
 import editIcon from "../../assets/icons/edit-24px.svg";
 import ChevronRight from "../../assets/icons/chevron_right-24px.svg";
 
-  
 
-const InventoryListCard=(
+const InventoryListCard=({inventoryList}
 )=>{
-
-    const id = 1;
-const itemName = "Television";
-const category = "Electronics";
-const status = "In Stock";
-const quantity = 500;
-const warehouseName = "Manhattan";
-
-// Function to handle the inventory modal toggle
-const handleInventoryModalToggle = () => {
-  // Your implementation here
-  console.log("Inventory modal toggled");
-};
-
-// Example inventory name for modal
-const inventoryNameForModal = "Samsung TV";
-
 
     return (
         <>
-      <li className="inventoryCard">
-        <div className="inventoryCard__container">
-          <div className="inventoryCard__left">
-            <div className="inventoryCard__left-item">
-              <p className="inventoryCard__left-itemTitle">INVENTORY ITEM</p>
-              <p className="inventoryCard__left-product">
-                <Link className="Link__item" to={`/itemDetails/${id}`}>
-                  {itemName}
-                  <img
-                    className="inventoryCard__left-productimg"
-                    src={ChevronRight}
-                    alt="drop down icon"
-                  />
-                </Link>
-              </p>
+        {
+          inventoryList?.map((item)=>(
+            <li className="inventoryCard" key={item.id}>
+            <div className="inventoryCard__container">
+              <div className="inventoryCard__left">
+                <div className="inventoryCard__left-item">
+                  <p className="inventoryCard__left-itemTitle">INVENTORY ITEM</p>
+                  <p className="inventoryCard__left-product">
+                    <Link className="Link__item" to={`/itemDetails/id`}>
+                      {item.item_name}
+                      <img
+                        className="inventoryCard__left-productimg"
+                        src={ChevronRight}
+                        alt="drop down icon"
+                      />
+                    </Link>
+                  </p>
+                </div>
+                <div className="inventoryCard__left-category">
+                  <p className="inventoryCard__left-categoryTitle">CATEGORY</p>
+                  <p className="inventoryCard__left-category">{item.category}</p>
+                </div>
+              </div>
+              <div className="inventoryCard__right">
+                <div className="inventoryCard__right-item">
+                  <p className="inventoryCard__itemTitle">STATUS</p>
+                  <p className="inventoryCard__item">{item.status}</p>
+                </div>
+                <div className="inventoryCard__left-category">
+                  <p className="inventoryCard__categoryTitle">QTY</p>
+                  <p className="inventoryCard__category">{item.quantity}</p>
+                </div>
+                <div className="inventoryCard__left-category">
+                  <p className="inventoryCard__categoryTitle">WAREHOUSE</p>
+                  <p className="inventoryCard__category">{item.warehouseName}</p>
+                </div>
+              </div>
             </div>
-            <div className="inventoryCard__left-category">
-              <p className="inventoryCard__left-categoryTitle">CATEGORY</p>
-              <p className="inventoryCard__left-category">{category}</p>
-            </div>
-          </div>
-          <div className="inventoryCard__right">
-            <div className="inventoryCard__right-item">
-              <p className="inventoryCard__itemTitle">STATUS</p>
-              <p className="inventoryCard__item">{status}</p>
-            </div>
-            <div className="inventoryCard__left-category">
-              <p className="inventoryCard__categoryTitle">QTY</p>
-              <p className="inventoryCard__category">{quantity}</p>
-            </div>
-            <div className="inventoryCard__left-category">
-              <p className="inventoryCard__categoryTitle">WAREHOUSE</p>
-              <p className="inventoryCard__category">{warehouseName}</p>
-            </div>
-          </div>
-        </div>
-        <div className="inventoryCard__icons">
-          <img
-            className="inventoryCard__left-icon"
-            src={deleteIcon}
-            alt="delete icon"
-          ></img>
-          <Link to="/editItem">
-            <img
-              className="inventoryCard__left-icon"
-              src={editIcon}
-              alt="edit icon"
-            ></img>
-          </Link>
-        </div>
-      </li>
-
-      {/* <li className="inventoryCardtableandDesktop__container">
-        <div className="inventoryCardtableandDesktop__list">
-          <h3 className="inventoryCardtableandDesktop__item">
-            <Link className="Link__item" to={`/itemDetails/${id}`}>
-              {itemName}
+            <div className="inventoryCard__icons">
               <img
-                className="inventoryCard__left-productimg"
-                src={ChevronRight}
-                alt="drop down icon"
+                className="inventoryCard__left-icon"
+                src={deleteIcon}
+                alt="delete icon"
               ></img>
-            </Link>
-          </h3>
+              <Link to="/editItem">
+                <img
+                  className="inventoryCard__left-icon"
+                  src={editIcon}
+                  alt="edit icon"
+                ></img>
+              </Link>
+            </div>
+          </li>
 
-          <h3 className="inventoryCardtableandDesktop__category">{category}</h3>
-          <h3 className="inventoryCardtableandDesktop__status">{status}</h3>
-          <h3 className="inventoryCardtableandDesktop__qty">{quantity}</h3>
-          <h3 className="inventoryCardtableandDesktop__warehouse">
-            {warehouseName}
-          </h3>
-          <div className="inventoryCardtableandDesktop__icons">
-            <img
-              className="inventoryCardtableandDesktop__icon"
-              onClick={() => {
-                handleInventoryModalToggle(id);
-                inventoryNameForModal(itemName);
-              }}
-              src={deleteIcon}
-              alt="delete icon"
-            ></img>
-            <Link to="/editItem">
-              <img
-                className="inventoryCardtableandDesktop__icon inventoryCardtableandDesktop__edit"
-                src={editIcon}
-                alt="edit icon"
-              ></img>
-            </Link>
-          </div>
-        </div>
-      </li> */}
-
+          ))
+        }
       
     </>
     )
