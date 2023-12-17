98efffe0f5c74de4b4adc18863d553b75d4b37ec
shanni6 <240686886@qq.com>
2023-12-16 22:16:11 -0800

    add warehouses component to warehousese page

--- src/pages/Warehouses/Warehouses.jsx
+++ src/pages/Warehouses/Warehouses.jsx
@@ -1,11 +1,12 @@
 import "./Warehouses.scss";
+import "./../../components/Warehouses/Warehouses";
 
 import { default as W } from "../../components/Warehouses/Warehouses";
 
 function Warehouses() {
     return (
         <div>
-            {/* Warehouses */}
+            <Warehouses />
             <W />
         </div>
     );
