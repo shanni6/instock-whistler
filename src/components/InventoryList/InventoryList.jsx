0c8418695ae84cadbcafa943a144ed076c1a7686
Jack Mladen <jackmladen1@gmail.com>
2023-12-17 18:58:02 -0800

    rough draft of Front-End: Delete Inventory Modal/Component (Responsive)

--- src/components/InventoryList/InventoryList.jsx
+++ src/components/InventoryList/InventoryList.jsx
@@ -4,6 +4,8 @@ import editIcon from "../../assets/icons/edit-24px.svg";
 import sortIcon from "../../assets/icons/sort-24px.svg";
 import searchIcon from "../../assets/icons/search-24px.svg";
 import InventoryListCard from "../InventoryListCard/InventoryListCard";
+import Dialog from "@mui/material/Dialog";
+import closeIcon from "../../assets/icons/close-24px.svg";
 
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
@@ -11,6 +13,8 @@ import axios from "axios";
 
 const InventoryList = () => {
   const [inventoryList, setInventoryList] = useState([]);
+
+  const [itemToDelete, setItemToDelete] = useState();
 
   const getList = async () => {
     try {
@@ -21,12 +25,53 @@ const InventoryList = () => {
     }
   };
 
+  const deleteItem = async (id) => {
+    try {
+      const response = await axios.delete(
+        `http://localhost:8080/api/inventories/${id}`
+      );
+      getList();
+      setItemToDelete();
+    } catch (error) {
+      console.error("Error deleting inventory item:", error);
+    }
+  };
+
   useEffect(() => {
     getList();
   }, []);
 
   return (
     <>
+      <Dialog onClose={() => setItemToDelete()} open={!!itemToDelete}>
+        <h1>Delete {itemToDelete?.item_name} inventory item?</h1>
+        <button
+          className="inventory__close-button"
+          onClick={() => setItemToDelete()}
+        >
+          <img src={closeIcon} alt="close-icon" />
+        </button>
+
+        <h2>
+          Please confirm that you’d like to delete Television from the inventory
+          list. You won’t be able to undo this action.
+        </h2>
+        <div className="inventory__buttons-container">
+          <button
+            className="inventory__cancel-button"
+            onClick={() => setItemToDelete()}
+          >
+            Cancel
+          </button>
+
+          <button
+            className="inventory__delete-button"
+            onClick={() => deleteItem(itemToDelete?.id)}
+          >
+            Delete
+          </button>
+        </div>
+      </Dialog>
       <section className="inventory">
         <div className="inventory__nav">
           <h1 className="inventory__title">Inventory</h1>
@@ -97,45 +142,50 @@ const InventoryList = () => {
           </ul>
           {/* ---------------- */}
 
-          {inventoryList?.map((item)=>(
+          {inventoryList?.map((item) => (
             <ul className="inventory__list" key={item.id}>
-            <Link to={`xxxxxxxxxxxxxxxx`}>
-            <li className="inventory__list-li product">
-              {item.item_name} &gt;
-            </li>
-            </Link>
-            
-            <li className="inventory__list-li">{item.category} </li>
-            <li className={`inventory__list-li inventory__list-stock ${item.status.toLowerCase() === 'out of stock' ? 'out-of-stock' : ''}`}>
-  {item.status}
-</li>
+              <Link to={`xxxxxxxxxxxxxxxx`}>
+                <li className="inventory__list-li product">
+                  {item.item_name} &gt;
+                </li>
+              </Link>
 
-            <li className="inventory__list-li inventory__list-quantity">
-              {item.quantity}
-            </li>
-            <li className="inventory__list-li inventory__list-location">
-              {item.warehouse_name}
-            </li>
-            <li className="inventory__list-li inventory__list-actions">
-              <img
-                src={deleteIcon}
-                className="inventory__deleteIcon"
-                alt="deleteIcon"
-              />
-              <img
-                src={editIcon}
-                className="inventory__editIcon"
-                alt="editIcon"
-              />
-            </li>
-          </ul>
-          ))
-          }
-          
+              <li className="inventory__list-li">{item.category} </li>
+              <li
+                className={`inventory__list-li inventory__list-stock ${
+                  item.status.toLowerCase() === "out of stock"
+                    ? "out-of-stock"
+                    : ""
+                }`}
+              >
+                {item.status}
+              </li>
+
+              <li className="inventory__list-li inventory__list-quantity">
+                {item.quantity}
+              </li>
+              <li className="inventory__list-li inventory__list-location">
+                {item.warehouse_name}
+              </li>
+              <li className="inventory__list-li inventory__list-actions">
+                <img
+                  src={deleteIcon}
+                  className="inventory__deleteIcon"
+                  alt="deleteIcon"
+                  onClick={() => setItemToDelete(item)}
+                />
+                <img
+                  src={editIcon}
+                  className="inventory__editIcon"
+                  alt="editIcon"
+                />
+              </li>
+            </ul>
+          ))}
         </div>
       </section>
 
-      <InventoryListCard inventoryList={inventoryList}/>
+      <InventoryListCard inventoryList={inventoryList} />
     </>
   );
 };
